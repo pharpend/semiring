344667e793e21e8470c9541aff6c84632b38bce4
Peter Harpending <pharpend2@gmail.com>
2014-05-06 19:55:10 -0600

    Moved code to the src/ directory, minor changes to API documentation.

--- src/Data/Semiring.hs
+++ src/Data/Semiring.hs
@@ -1,11 +1,12 @@
--- | 
--- Module      : Data.Semiring
--- Copyright   : Thomas Wilke, Frank Huch, Sebastian Fischer, Peter Harpending
--- License     : BSD3
--- Maintainer  : Peter Harpending <pharpend2@gmail.com>
--- 
--- This library provides a type class for semirings.
--- 
+{-| 
+Module      : Data.Semiring
+Copyright   : Thomas Wilke, Frank Huch, Sebastian Fischer, Peter Harpending
+License     : BSD3
+Maintainer  : Peter Harpending <pharpend2@gmail.com>
+
+This library provides a type class for semirings.
+
+-}
 
 module Data.Semiring where
 
@@ -31,20 +32,6 @@ module Data.Semiring where
 -- 
 -- > zero .*. a  ==  zero
 -- > a .*. zero  ==  zero
--- 
--- All laws should hold with respect to the required `Eq` instance.
--- 
--- For example, the Booleans form a semiring.
--- 
---  * @False@ is an identity of disjunction which is commutative and
---    associative,
--- 
---  * @True@ is an identity of conjunction which is associative,
--- 
---  * conjunction distributes over disjunction, and
--- 
---  * @False@ annihilates the Booleans with respect to conjunction.
--- 
 class (Eq s) => Semiring s where
   zero, one    :: s
   (.+.), (.*.) :: s -> s -> s
